ae2744187ed1c0adef4c5d7f72e66714ebebca58
lordz-md <lordz.md@gmail.com>
2019-11-26 16:14:59 +0200

    New instance added

--- instance.tf
+++ instance.tf
@@ -13,3 +13,14 @@ subnet_id = aws_subnet.main-public-1.id
 # the security group
 vpc_security_group_ids = [aws_security_group.allow-ssh.id]
 }
+
+resource "aws_instance" "example2" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.mykey.key_name
+  # the VPC subnet
+subnet_id = aws_subnet.main-public-2.id
+
+# the security group
+vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+}
