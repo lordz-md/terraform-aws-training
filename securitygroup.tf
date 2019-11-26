4a0d18e3fa38a83d2854e245f0b5998d9dd2e342
lordz-md <lordz.md@gmail.com>
2019-11-26 16:24:01 +0200

    update security group

--- securitygroup.tf
+++ securitygroup.tf
@@ -18,6 +18,18 @@ resource "aws_security_group" "allow-ssh" {
   tags = {
     Name = "allow-ssh"
   }
+}
+
+resource "aws_security_group" "allow-http" {
+  vpc_id      = aws_vpc.main.id
+  name        = "allow-http"
+  description = "security group that allows ssh and all egress traffic"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 80
