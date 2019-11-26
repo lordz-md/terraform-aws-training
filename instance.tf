fc7ad12b8cd95c80e1b2b7e2e2ef953a00d3e6b0
lordz-md <lordz.md@gmail.com>
2019-11-26 16:11:05 +0200

    This is the new AWS training repo

---
+++ instance.tf
@@ -0,0 +1,15 @@
+resource "aws_key_pair" "mykey" {
+  key_name   = "tkey"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
+resource "aws_instance" "example" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.mykey.key_name
+  # the VPC subnet
+subnet_id = aws_subnet.main-public-1.id
+
+# the security group
+vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+}
