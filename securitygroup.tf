ae5e1538524ed31dffc6df049e8990b2b23b090f
lordz-md <lordz.md@gmail.com>
2019-11-26 16:25:11 +0200

    Allow only Endava IP

--- securitygroup.tf
+++ securitygroup.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "allow-ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["194.126.146.46/32"]
   }
   tags = {
     Name = "allow-ssh"
